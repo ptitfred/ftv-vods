ffe32d02a58dd221eb7676e529dfa2bdf967f739
Frédéric Menou <frederic.menou@gmail.com>
2016-05-09 09:25:55 +0200

    Refactoring of big functions

--- src/Liquipedia.hs
+++ src/Liquipedia.hs
@@ -22,17 +22,27 @@ fuse :: [Tournament] -> [Tournament] -> [Tournament]
 fuse premiers alls = premiers ++ deleteFirstsBy (\t1 t2 -> tournamentURL t1 == tournamentURL t2) alls premiers
 
 getTournaments :: TournamentType -> IO (Maybe [Tournament])
-getTournaments tournamentType = do
-  graph <- getRDF tournamentType
-  let nodes = map subjectOf $ query graph Nothing (someUrl "rdf:type") (someUrl $ category tournamentType)
-  return $ Just $ map (searchTournament graph) nodes
-  where someUrl = Just . unode . T.pack
-        category Premier = "&wiki;Category-3APremier_Tournaments"
-        category Standard = "&wiki;Category-3ATournaments"
-        searchTournament g n = Tournament (searchText g n) (searchURL g n) tournamentType
-        searchText g n = map unslash $ readText $ searchProperty "rdfs:label" g n
-        searchURL g n = readURL $ searchProperty "swivt:page" g n
-        searchProperty url g n = head $ map objectOf $ query g (Just n) (someUrl url) Nothing
+getTournaments tournamentType =
+  readTournaments tournamentType <$> getRDF tournamentType
+
+readTournaments :: RDF rdf => TournamentType -> rdf -> Maybe [Tournament]
+readTournaments tournamentType graph =
+  Just $ map (searchTournament graph) nodes
+    where nodes = map subjectOf $ query graph Nothing withType (category tournamentType)
+          searchTournament g n = Tournament (searchText g n) (searchURL g n) tournamentType
+          searchText g n = map unslash $ readText $ searchProperty "rdfs:label" g n
+          searchURL g n = readURL $ searchProperty "swivt:page" g n
+          searchProperty url g n = head $ map objectOf $ query g (Just n) (someUrl url) Nothing
+
+withType :: Maybe Node
+withType = someUrl "rdf:type"
+
+someUrl :: String -> Maybe Node
+someUrl = Just . unode . T.pack
+
+category :: TournamentType -> Maybe Node
+category Premier  = someUrl "&wiki;Category-3APremier_Tournaments"
+category Standard = someUrl "&wiki;Category-3ATournaments"
 
 unslash :: Char -> Char
 unslash '/' = ' '
